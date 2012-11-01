5109e7dd4e93bff4d6243dbc09ea482a2e386524
Simon Rydberg <simpor81@gmail.com>
2012-11-01 21:39:59 +0100

    Projekt start

---
+++ src/main/java/se/simpor/urbanterror/logparser/Test.java
@@ -0,0 +1,11 @@
+package se.simpor.urbanterror.logparser;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: simon.rydberg
+ * Date: 2012-11-01
+ * Time: 21:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
---
+++ src/main/scala/se/simpor/urbanterror/logparser/TestScala.scala
@@ -0,0 +1,12 @@
+package se.simpor.urbanterror.logparser
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: simon.rydberg
+ * Date: 2012-11-01
+ * Time: 21:36
+ * To change this template use File | Settings | File Templates.
+ */
+class TestScala {
+
+}
