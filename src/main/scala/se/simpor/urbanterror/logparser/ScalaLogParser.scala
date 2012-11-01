fa0785c6cb9b4ebd7e711c4a6f4c92f9ce16417e
Simon Rydberg <simpor81@gmail.com>
2012-11-02 00:18:31 +0100

    Börjat parsa i scala

--- src/main/scala/se/simpor/urbanterror/logparser/ScalaLogParser.scala
+++ src/main/scala/se/simpor/urbanterror/logparser/ScalaLogParser.scala
@@ -1,5 +1,7 @@
 package se.simpor.urbanterror.logparser
 
+import collection.mutable
+
 /**
  * Created with IntelliJ IDEA.
  * User: simon.rydberg
@@ -11,18 +13,18 @@ object ScalaLogParser {
 
   class Player {
     var name = ""
-    var kill = 0
-    var deaths = 0
+    var killedBy = mutable.ListBuffer[Player]()
+    var killedPlayer = mutable.ListBuffer[Player]()
+
   }
 
-
+  var players = new mutable.HashMap[String, Player]
 
   def parseLog(logFile: String) {
     val source = scala.io.Source.fromFile(logFile)
     val lines: Iterator[String] = source.getLines
 
     for (row <- lines) {
-
       row match {
         case x if row.contains("InitRound:") => initRound(row)
         case x if row.contains("Hit:") => hit(row)
@@ -32,6 +34,12 @@ object ScalaLogParser {
       }
     }
     source.close()
+
+
+    for ((key, value) <- players) {
+
+      println(value.name + ", killed: " + value.killedPlayer.length + ", deaths: " + value.killedBy.length)
+    }
   }
 
   def initRound(row: String) {
@@ -40,6 +48,25 @@ object ScalaLogParser {
 
   def kill(row: String) {
     println(row)
+    val split = row.split("\\:")
+    val text = split(3)
+    val info = text.split("(killed)|(by)")
+
+    val player1 = getPlayer(info(0))
+    val player2 = getPlayer(info(1))
+
+    if (!info(2).contains("MOD_CHANGE_TEAM")) {
+      player1.killedPlayer += player2
+      player2.killedBy += player1
+    }
+  }
+
+  def getPlayer(player: String): Player = {
+    if (!(players contains player)) {
+      players += player -> new Player
+      players(player).name = player.trim
+    }
+    players(player)
   }
 
   def hit(row: String) {
